94a047d8eca281821af222088d6ff508f33ea1a1
JackDotJS <jackdotjs@proton.me>
2023-11-08 06:27:13 -0800

    update global styles, add quick toggle, options button

--- popup-options/popup.js
+++ popup-options/popup.js
@@ -1,4 +1,10 @@
 const manifest = browser.runtime.getManifest();
 const versionElem = document.getElementById(`ext-version`);
+const optionsButton = document.getElementById(`open-options`);
 
-versionElem.innerHTML = `Version: ${manifest.version}`;
+versionElem.innerHTML = `Version ${manifest.version}`;
+
+optionsButton.addEventListener(`click`, () => {
+  browser.runtime.openOptionsPage();
+  window.close();
+});
