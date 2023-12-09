8a8711bec42ef9264a0ec8eaa9a9618e48a6c8d2
JackDotJS <jackdotjs@proton.me>
2023-12-08 16:21:56 -0800

    popup: quick toggle switch state is now saved

--- popup-options/popup.js
+++ popup-options/popup.js
@@ -1,8 +1,26 @@
+// quick toggle switch
+
+const enabledElem = document.getElementById(`quicktoggle`);
+
+browser.storage.local.get(`extensionEnabled`).then((opts) => {
+  enabledElem.checked = opts.extensionEnabled;
+});
+
+enabledElem.addEventListener(`change`, (e) => {
+  browser.storage.local.set({
+    extensionEnabled: e.target.checked
+  });
+});
+
+// extension version
+
 const manifest = browser.runtime.getManifest();
 const versionElem = document.getElementById(`ext-version`);
 const optionsButton = document.getElementById(`open-options`);
 
 versionElem.innerHTML = `Version ${manifest.version}`;
+
+// options page button
 
 optionsButton.addEventListener(`click`, () => {
   browser.runtime.openOptionsPage();
